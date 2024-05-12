20fe2fd824106a8b4b1ceaa92e61de5cf208500c
supsokman <supsokman@gmail.com>
2024-05-13 08:51:04 +0900

    pwa

--- js/main.js
+++ js/main.js
@@ -96,3 +96,14 @@ var swiperRentalGoods = new Swiper(".rental-swiperGoods", {
     clickable: true,
   },
 });
+
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/service-worker.js')
+    .then(function(registration) {
+      console.log('Service Worker registration successful with scope: ', registration.scope);
+    })
+    .catch(function(error) {
+      console.log('Service Worker registration failed: ', error);
+    });
+}
